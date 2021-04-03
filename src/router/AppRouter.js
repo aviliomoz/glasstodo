49dc607d3c161d4f734bd91550ed8760b530f9b3
Avilio Muñoz <aviliomuoz@gmail.com>
2021-04-02 23:05:02 -0500

    Github pages

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Redirect } from 'react-router-dom';
 import { Loading } from '../components/Loading';
 import { renew } from '../redux/actions/authActions';
 
